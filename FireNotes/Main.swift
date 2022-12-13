916bc08f0fc89d69ce0bbf1a2de69c7d5c68c610
jdeda <jldeda00@gmail.com>
2022-12-13 12:59:28 -0500

    initial setup

--- FireNotes/Main.swift
+++ FireNotes/Main.swift
@@ -1,17 +1,10 @@
-//
-//  FireNotesApp.swift
-//  FireNotes
-//
-//  Created by Jesse Deda on 12/13/22.
-//
-
 import SwiftUI
 
 @main
 struct FireNotesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppView()
         }
     }
 }
