dee3d125b7143ca82b895e515ab4e7681c882aa8
jdeda <jldeda00@gmail.com>
2022-12-13 12:54:16 -0500

    Initial Commit

---
+++ FireNotes/FireNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireNotesApp.swift
+//  FireNotes
+//
+//  Created by Jesse Deda on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct FireNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
